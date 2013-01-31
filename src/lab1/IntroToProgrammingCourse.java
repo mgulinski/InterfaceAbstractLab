c5ce1684475f16e6075b5c158d51ed2e81eaaf45
mgulinski@my.wctc.edu <>
2013-01-31 16:28:35 -0600

    first commit - started, superclass added

---
+++ src/lab1/IntroToProgrammingCourse.java
@@ -0,0 +1,21 @@
+package lab1;
+
+/**
+ * Describe responsibilities here. NOTE: this class has NO PREREQUISITES!
+ * Do not change this fact.
+ *
+ * @author      Maciej
+ * @version     1.00
+ */
+public class IntroToProgrammingCourse extends ProgrammingCourse {
+    
+
+    public IntroToProgrammingCourse(String courseName, String courseNumber) {
+       super(courseName, courseNumber);
+      
+    }
+
+    
+   
+    
+}
